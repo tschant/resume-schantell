bac86cf6af968437ba32a30cfd7173cf817e3c09
Tarryn <tarryn@pop-os.localdomain>
2023-02-09 07:03:46 -0600

    Add amplify hosting config

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,10 @@
+export type AmplifyDependentResourcesAttributes = {
+  "hosting": {
+    "S3AndCloudFront": {
+      "HostingBucketName": "string",
+      "Region": "string",
+      "S3BucketSecureURL": "string",
+      "WebsiteURL": "string"
+    }
+  }
+}
